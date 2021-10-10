c6f6c4326a044eff9def07417e3d9df498d949ba
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-10-11 05:51:25 +0900

    introduce bootstrap

--- handlers.go
+++ handlers.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"text/template"
 )
 
 // Home is the home page handler
@@ -14,13 +12,4 @@ func Home(w http.ResponseWriter, r *http.Request) {
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
-	}
 }
